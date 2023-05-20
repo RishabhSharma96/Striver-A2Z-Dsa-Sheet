7953f04f287a4c029e678007c391432e79eff1b2
RishabhSharma96 <114917946+RishabhSharma96@users.noreply.github.com>
2023-05-20 13:50:38 +0530

    max_sum_subarray

---
+++ Arrays/maximum_sum_subarray.cpp
@@ -0,0 +1,23 @@
+// KADANES ALGORITHM
+
+class Solution {
+    
+public:
+    int maxSubArray(vector<int>& nums) {
+        long long sum = 0 ;
+        long long maxi = INT_MIN ;
+
+        for(int i=0 ; i <nums.size() ; i++){
+            sum += nums[i] ;
+            if(sum > maxi){
+                maxi = sum ;
+            }
+
+            if(sum < 0){
+                sum = 0; 
+            }
+        }
+
+        return maxi ;
+    }
+};
